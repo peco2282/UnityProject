fc1b5108d37b44f75002d3750801782b15f45941
peco2282 <87461070+peco2282@users.noreply.github.com>
2023-06-27 20:12:27 +0900

    move left & right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
+[SuppressMessage("ReSharper", "IdentifierTypo")]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  [SerializeField] private Animator animator;
+
+  private bool _isMoving;
+
+  [SerializeField] private float speed;
+
+  private float _hval, _vval;
+  private static readonly int IsMove = Animator.StringToHash("isMove");
+  private static readonly int Goal = Animator.StringToHash("Goal");
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    _hval    = Input.GetAxis("Horizontal");
+    _vval    = Input.GetAxis("Vertical");
+    animator = GetComponent<Animator>();
+    if (animator == null) Debug.Log("Animation component cannot find.");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var state = false;
+    if (Input.GetKey(KeyCode.LeftArrow))
     {
-        
+      state = true;
+      transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
     }
 
-    // Update is called once per frame
-    void Update()
+    if (Input.GetKey(KeyCode.RightArrow))
     {
-        
+      state              =  true;
+      transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
     }
+
+    if (Input.GetKey(KeyCode.UpArrow))
+    {
+      state              =  true;
+      transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+    }
+
+    animator.SetBool(IsMove, state);
+  }
+
+  void OnCollisionEnter(Collision other)
+  {
+    Debug.Log(other);
+    Debug.Log("collisionenter.");
+  }
 }
